33ce6a09f8948d9aa121704f312447171cc09263
Michal Kreft <michal.kreft@softwire.com>
2020-06-27 13:49:51 +0100

    Handle negative fibbonaci

--- src/main/kotlin/com/gitTraining/Fibbonaci.kt
+++ src/main/kotlin/com/gitTraining/Fibbonaci.kt
@@ -1,6 +1,11 @@
 package com.gitTraining
 
 fun computeFibbonaciNumber(position: Int): Int {
+    if (position == 0) return 0
+    if (position < 0) {
+        val positionIsOdd = position % 2 == -1
+        return if (positionIsOdd) computeFibbonaciNumber(-position) else (computeFibbonaciNumber(-position) * -1)
+    }
     var i = 1
     var j = 1
 
