853244111e8ff7686d52ffc1bdb20716b8b2e418
Michal Kreft <michal.kreft@softwire.com>
2020-06-27 14:11:03 +0100

    Made computing more readable

--- src/main/kotlin/com/gitTraining/Fibbonaci.kt
+++ src/main/kotlin/com/gitTraining/Fibbonaci.kt
@@ -5,19 +5,20 @@ fun computeFibbonaciNumber(position: Int): Int {
     if (position < 0) {
         return computeNegativeFibbonachi(position)
     }
-    var i = 1
-    var j = 1
 
-    if (position <= 2) return 1
+    if (position == 1 || position == 2) return 1
+
+    var smallFibbonachiNumber = 1
+    var largeFibbonachiNumber = 1
 
     var currentPosition = 2
     while (currentPosition < position) {
-        val temp = i
-        i = j
-        j += temp
+        val nextFibbonachiNumber = smallFibbonachiNumber + largeFibbonachiNumber
+        smallFibbonachiNumber = largeFibbonachiNumber
+        largeFibbonachiNumber = nextFibbonachiNumber
         currentPosition ++
     }
-    return j
+    return largeFibbonachiNumber
 }
 
 fun computeNegativeFibbonachi(position:Int): Int {
